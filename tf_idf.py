b055828c56fc84c06896609dd1a6aec1706b9ed3
kongriley <kongriley@gmail.com>
2017-10-28 10:56:34 -0700

    Start TF-IDF with articles

--- tf_idf.py
+++ tf_idf.py
@@ -1,7 +1,21 @@
-from gensim.corpora import MmCorpus
+import glob
+
+import re
+from gensim.corpora import MmCorpus, Dictionary
 from gensim.models.tfidfmodel import *
+from gensim.models import TfidfModel
 
+texts = []
 
+for fname in glob.glob('data/raw/news.*'):
+    print('Starting', fname)
+
+    with open(fname, 'r') as f:
+        data = re.sub("[^a-zA-Z\s.]", " ", f.read()).split()
+        texts.append(data)
+
+print(texts)
+dictionary = Dictionary(texts)
 corpus = MmCorpus('data/text.mm')
 
 tfidf = TfidfModel(corpus)
