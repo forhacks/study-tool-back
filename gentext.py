f84baa4b9eaa6f99ce76f8ef4f7ab7a643814809
Oliver Ni <oliver.ni@gmail.com>
2017-10-27 21:22:14 -0700

    Add debug log
    Add print statement to gentext

--- gentext.py
+++ gentext.py
@@ -6,6 +6,7 @@ path = "data/raw/news.*"
 fout = open('data/text.txt', 'w')
 
 for fname in glob.glob(path):
+    print('Starting', fname)
     with open(fname, 'r') as f:
         data = f.read()
         text = re.sub("[^a-zA-Z\s.]", " ", data)
