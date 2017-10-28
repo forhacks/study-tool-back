d0ab335e51f5927a9eccc4f52bcac91a04b209db
thegithubgeek <wfrankw9@gmail.com>
2017-10-27 21:45:56 -0700

    Edit main

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import numpy as np
 import re
 
 # variables
-# tfidf = TfidfModel.load("trained/tfidf.model")
+tfidf = TfidfModel.load("trained/tfidf.model")
 w2v = Word2Vec.load("trained/trained.w2v")
 articles = ["a", "an", "the"]
 threshold = 0.9
 
+
 def compare(def1, def2):
+
+    # process words and split into array
     def1 = re.sub("[^a-zA-Z\s]", " ", def1.lower()).split()
     def2 = re.sub("[^a-zA-Z\s]", " ", def2.lower()).split()
     def1 = [x for x in def1 if not articles.__contains__(x)]
@@ -20,5 +23,16 @@ def compare(def1, def2):
     def2v = w2v[def2]
 
     # weights of each word
+    def1w = tfidf[def1]
+    def2w = tfidf[def2]
+
+    # take dot product of vector and weight
+    def1v = np.dot(def1v, def1w)
+    def2v = np.dot(def2v, def2w)
+
+    # take cos distance
+    difference = np.inner(def1v, def2v)
+
+    return difference
 
 compare("This is a test", "Test this is")
